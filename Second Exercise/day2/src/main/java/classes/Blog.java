04264a7bdf0005e3e12f0fd0abe6df50e0996ae0
LeandroAsano <leandroasano@gmail.com>
2018-09-20 14:12:46 -0300

    day-2

--- Second Exercise/day2/src/main/java/classes/Blog.java
+++ Second Exercise/day2/src/main/java/classes/Blog.java
@@ -1,45 +1,64 @@
 package classes;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Blog {
 
-    static void newEntry(List<Entry> entries){
+    //this method is like a function to show the Tags existents
+    public static void showTags(HashMap<Integer,String> tagNames){
+        for (int i = 0; i < tagNames.size(); i++) {
+            System.out.println((i+1)+"-#"+tagNames.get(i+1));
+        }
+
+    }
+
+    //This method post new entries for the blog
+    private static void newEntry(List<Entry> entries, HashMap<Integer,String> tagNames){
         Scanner input = new Scanner(System.in);
         String title;
         String text;
-        String tags;
+        String tags = "";
+        int tagIn;
 
+        //input of variables
         System.out.println("Enter the title ");
         title=input.nextLine();
         System.out.println("Enter text");
         text=input.nextLine();
-        System.out.println("Enter tags ");
-        tags=input.nextLine();
-
+        System.out.println("Select tags ");
+        showTags(tagNames);
+        System.out.println("0-Finish");
+        tagIn=input.nextInt();
+        //this cicle works while the user keeps selecting tags
+        while (tagIn!=0){
+            tags = tags.concat(" #"+tagNames.get(tagIn)); //this concat the tags selected by the user
+            tagIn=input.nextInt();
+        }
+        //Date and List creation
         LocalDate date = LocalDate.now();
         Entry ent = new Entry(title,text,date,tags);
-        entries.add(ent);
+        entries.add(ent);                           //Charge of entry to the List
 
         System.out.println("Entry Successfully!");
     }
 
-    static void deleteEntry(List<Entry> entries){
+    private static void deleteEntry(List<Entry> entries){
         Scanner input = new Scanner(System.in);
         int cont=1;
         int choice;
         System.out.println("Which entry you want delete?");
+        //this for allow show the entries
         for (Entry p: entries) {
 
             System.out.println(cont+"-"+p.getEntry());
             cont++;
         }
+        //input and control of the choice selected for delete
         do {
                 choice = input.nextInt();
         }while(choice<1 || choice >4);
+        //this try catch if for control of the existence of choice
         try {
             entries.remove(choice-1);
             System.out.println("Entry Removed Successfully!");
@@ -48,31 +67,52 @@ public class Blog {
         }
     }
 
-    static void showTenRecentEntries(List<Entry> entries){
+    private static void showTenRecentEntries(List<Entry> entries){
+        //show the entries
         for (int i = 0; i < 10 ; i++) {
+            //this try catch allow show "Empty" if not exists 10 entries now.
            try {
                System.out.println((i+1)+"-"+entries.get(i).getEntry());
            } catch (Exception e){
-               System.out.println((i+1)+"-  Vacio");
+               System.out.println((i+1)+"- Empty");
            }
         }
 
     }
 
+    public static void defineTags(HashMap<Integer, String> tagNames, int cont){
+        Scanner input = new Scanner(System.in);
+        String tagName;
+        //input of variables
+        showTags(tagNames);
+        System.out.println();
+        System.out.println(" Enter tagName: ");
+        tagName=input.next();
+        tagNames.put((cont+1),tagName); //put of the tag in the HashMap
+        System.out.println("TagName Registered!");
+    }
+
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
         int option;
+        int cont=0;
 
+        //instance of the HashMap and List Collections
+        HashMap<Integer,String> tagNames = new HashMap<>();
         List<Entry> entries = new ArrayList<>();
+
+        //This cicle executes while option are distinct of "Exit"
         do {
-            System.out.println("WELCOME TO DEVSBLOG \n 1-New Entry \n 2-Delete Entry \n 3-Show 10 Recent Entries \n 4-Exit");
+            System.out.println("WELCOME TO DEVSBLOG \n 1-New Entry \n 2-Delete Entry \n 3-Show 10 Recent Entries \n 4-Define tags");
+            System.out.println(" 5-Exit");
+            //control of valid number
             do {
                     option = input.nextInt();
-            }while (option < 1 || option > 4);
+            }while (option < 1 || option > 5);
 
             switch (option) {
                 case 1: {
-                    newEntry(entries);
+                    newEntry(entries,tagNames);
                 }
                 break;
                 case 2: {
@@ -82,9 +122,13 @@ public class Blog {
                 case 3: {
                     showTenRecentEntries(entries);
                 }break;
+                case 4: {
+                    defineTags(tagNames,cont);
+                    cont++;
+                }
             }
 
-        }while(option!=4);
+        }while(option!=5);
     }
 
 }
